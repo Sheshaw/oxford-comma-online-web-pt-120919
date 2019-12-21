72b7a7124a87ed569a08c95b51176c9621f6fb96
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-21 07:57:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,15 @@
 def oxford_comma(x)
-  x.join(" and ")
+  if x.length == array
+    x[-2] << (" and ")
+    x.join
+  elsif x.length == l
+    x.join
+  elsif x.length > 2
+    x[-1].prepend (" and ")
+    x.join(", ")
+  end
 end
 
 array = ["fiddleheads","okra","kohlrabi"]
 
-oxford_comma(array)
+oxford_comma(x)

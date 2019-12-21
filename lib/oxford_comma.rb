ece8534b40b03e0d2042417f2495e122eb9d5f21
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-21 08:01:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == l
     array.join
-  elsif x.length > 2
+  elsif array.length > 2
     array[-1].prepend " and "
     array.join(", ")
   end

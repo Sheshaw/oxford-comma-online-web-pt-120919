8c44569fdfb543a23affff282aaf40ca7913d916
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-21 07:59:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(x)
   if x.length == array
-    x[-2] << (" and ")
+    x[-2] << " and "
     x.join
   elsif x.length == l
     x.join
   elsif x.length > 2
-    x[-1].prepend (" and ")
+    x[-1].prepend " and "
     x.join(", ")
   end
 end
